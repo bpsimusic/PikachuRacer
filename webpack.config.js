62f070a3e954dc9e5233e8ae8ddf9b8bfb727efa
bpsimusic <bpsimusic@gmail.com>
2016-11-14 15:28:41 -0800

    Added a background image using Canvas

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './lib/pikachu.js',
+  output: {
+    path: './lib',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx' ]
+  }
+};
